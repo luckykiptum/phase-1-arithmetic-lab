5c1315d013d87d54163bc12f1bf3ea145f3cbeb0
lucky <luckykiptum@gmail.com>
2024-03-19 14:16:44 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
 
-// Write your code here
+let num2 = 2;
+
+let multiply = num1 * num2 ;{
+
+console.log( multiply);
+}
+
+let random =Math.floor(Math.random() * 10) + 1;
+
+let num3  = 68;
+let num4 = 8;
+
+let mod = num3 % num4 ;{
+    console.log(mod)
+}
+
+let max = Math.max(20,17,14,5,6,8,5,19,18,9);{
+    console.log
+}
